bfdac896f27c009e4001d854992c2430b89b06b1
sumitgit01 <sumitjoshi1988@gmail.com>
2025-03-02 18:30:02 +0530

    pusing artifact into nexus

--- vars/build_and_deployment_SIT.groovy
+++ vars/build_and_deployment_SIT.groovy
@@ -1,6 +1,13 @@
 def call() {
     pipeline {
         agent any
+        environment {
+            NEXUS_VERSION = "nexus3"
+            NEXUS_PROTOCOL = "http"
+            NEXUS_URL = "http://192.168.18.11:8081"
+            NEXUS_REPOSITORY = "seh_students"
+            NEXUS_CREDENTIAL_ID = "nexusCredential"
+        }
         stages {
             stage('Build_Provisioning'){
                 steps { 
@@ -15,6 +22,41 @@ def call() {
                             ls -lart target/
                         """
                    }
+                }
+            }
+            stage('Push Artifacts to Nexus') {
+                    steps {
+                        script {
+                    pom = readMavenPom file: "pom.xml";
+                    filesByGlob = findFiles(glob: "target/*.${pom.packaging}");
+                    echo "${filesByGlob[0].name} ${filesByGlob[0].path} ${filesByGlob[0].directory} ${filesByGlob[0].length} ${filesByGlob[0].lastModified}"
+                    artifactPath = filesByGlob[0].path;
+                    artifactExists = fileExists artifactPath;
+                    if(artifactExists) {
+                        echo "*** File: ${artifactPath}, group: ${pom.groupId}, packaging: ${pom.packaging}, version ${pom.version}";
+                        nexusArtifactUploader(
+                            nexusVersion: NEXUS_VERSION,
+                            protocol: NEXUS_PROTOCOL,
+                            nexusUrl: NEXUS_URL,
+                            groupId: pom.groupId,
+                            version: pom.version,
+                            repository: NEXUS_REPOSITORY,
+                            credentialsId: NEXUS_CREDENTIAL_ID,
+                            artifacts: [
+                                [artifactId: pom.artifactId,
+                                classifier: '',
+                                file: artifactPath,
+                                type: pom.packaging],
+                                [artifactId: pom.artifactId,
+                                classifier: '',
+                                file: "pom.xml",
+                                type: "pom"]
+                            ]
+                        );
+                    } else {
+                        error "*** File: ${artifactPath}, could not be found"; 
+                        }
+                    }
                 }
             }
         }
